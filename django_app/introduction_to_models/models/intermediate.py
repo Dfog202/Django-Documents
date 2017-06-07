571ed08f594d6466845f352b37701fbf18be3eb0
Seungha Lee <poopa8788@gmail.com>
2017-06-07 11:52:24 +0900

    과제 풀이

--- django_app/introduction_to_models/models/intermediate.py
+++ django_app/introduction_to_models/models/intermediate.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.db.models import Q
 
 
 class Player(models.Model):
@@ -11,19 +11,20 @@ class Player(models.Model):
     # current_club프로퍼티에 현재 속하는 Club 리턴
     @property
     def current_club(self):
-        return self.club_set.get(tradeinfo__date_leaved__isnull=True).name
+        return self.current_tradeinfo.club
 
     # current_tradeinfo프로퍼티에 현재 자신의 TradeInfo리턴
     @property
     def current_tradeinfo(self):
-        return TradeInfo.objects.get(player=self)
+        return self.tradeinfo_set.get(tradeinfo__date_leaved__isnull=True)
+
 
 class Club(models.Model):
     name = models.CharField(max_length=40)
     players = models.ManyToManyField(
         Player,
         through='TradeInfo',
-        # through_fields=('player', 'club')
+        through_fields=('club', 'player'),
     )
 
     def __str__(self):
@@ -31,14 +32,19 @@ class Club(models.Model):
 
     def squad(self, year=None):
         if year:
-            self.players.filter(
-                tradeinfo__date_joined__lte=(year, 1, 1),
-                # tradeinfo__date_leaved__lte=(year, 12, 31),
+            # 인수로 년도(2017, 2015 등)을 받아 해당 년도의 현직 선수들을 리턴,
+            # 떠난 날짜가 2015. 1. 1보다는 커야한다
+            # 들어온 날짜는 2016. 1. 1보다는 작아야한다
+            return self.players.filter(
+                Q(tradeinfo__date_joined__lt=datetime(year + 1, 1, 1)) &
+                (
+                    Q(tradeinfo__date_leaved__gt=datetime(year, 1, 1)) |
+                    Q(tradeinfo__date_deaved__isnull=True)
+                )
             )
         else:
-            self.players.filter(tradeinfo__date_joined__lte=timezone.now())
             # squad 매서드에 현직 선수들만 리턴
-            # 인수로 년도(2017, 2015 등)을 받아 해당 년도의 현직 선수들을 리턴,
+            return self.players.filter(tradeinfo__date_leaved__isnull=True)
             # 주어지지지 않으면 현재를 기준으로 함
 
 
@@ -47,12 +53,34 @@ class TradeInfo(models.Model):
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     date_joined = models.DateField()
     date_leaved = models.DateField(null=True, blank=True)
-    # recommender = models.ForeignKey(Player, on_delete=models.CASCADE, null=True, blank=True)
+    recommender = models.ForeignKey(
+        Player,
+        on_delete=models.PROTECT,
+        related_name='tradeinfo_set_by_recommender',
+        null=True,
+        blank=True,
+    )
     # prev_club = 이전 Club
-    # prev_club = models.ForeignKey(Club, on_delete=models.CASCADE, null=True, blank=True)
+    prev_club = models.ForeignKey(
+        Club,
+        on_delete=models.PROTECT,
+        related_name='+',
+        null=True,
+        blank=True,
+    )
 
     # property로 is_current 속성이 TradeInfo가 현재 현직(leaved하지 않았는지)
     # recommender와 prev_club을 활성화 시키고 Club의 MTM필드에 through_fields를 명시
     @property
     def is_current(self):
-        return self.date_leaved is None
+        # return self.date_leaved is None
+        return not self.date_leaved
+
+    def __str__(self):
+        # 선수이름, 구단명 (시작일자 ~ 종료일자)
+        return '{}, {} ({} ~ {})'.format(
+            self.player.name,
+            self.club.name,
+            self.date_joined,
+            self.date_leaved if self.date_leaved else '현직',
+        )
